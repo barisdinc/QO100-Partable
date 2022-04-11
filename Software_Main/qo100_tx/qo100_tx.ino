dd0f23e619d48a5ef14cfdd96ba4764b04256929
barisdinc <barisdinc@gmail.com>
2022-04-11 23:40:26 +0300

    tx tx tx

---
+++ Software_Main/qo100_tx/qo100_tx.ino
@@ -0,0 +1,362 @@
+
+//#include <avr/sleep.h>
+#include <avr/wdt.h>
+
+//FUSES = { .low = 0xFF, .high = 0xD6, .extended = 0xFD };   // Fuse settings should be set at programming (Arduino IDE > Tools > Burn bootloader)
+
+
+int test_count = 0;
+int test_dsp_count = 0;
+int max_adc = 0;
+
+#define F_MCU 20000000  // 20MHz ATMEGA328P crystal
+
+//#define log2(n) (log(n) / log(2))
+uint8_t log2(uint16_t x){
+  uint8_t y = 0;
+  for(; x>>=1;) y++;
+  return y;
+}
+
+enum dsp_cap_t { ANALOG, DSP, SDR };
+const uint8_t ssb_cap = 1;
+const uint8_t dsp_cap = SDR;
+
+enum mode_t { LSB, USB, CW, FM, AM };
+volatile uint8_t mode = USB;
+volatile uint16_t numSamples = 0;
+
+volatile int32_t freq = 14000000;
+static int32_t vfo[] = { 7074000, 14074000 };
+static uint8_t vfomode[] = { USB, USB };
+
+#define F_SAMP_TX 4800 //4810 //4805 // 4402 // (Design) ADC sample-rate; is best a multiple of _UA and fits exactly in OCR2A = ((F_CPU / 64) / F_SAMP_TX) - 1 , should not exceed CPU utilization
+#define _UA  600 //=(_FSAMP_TX)/8 //(_F_SAMP_TX)      //360  // unit angle; integer representation of one full circle turn or 2pi radials or 360 degrees, should be a integer divider of F_SAMP_TX and maximized to have higest precision
+#define MAX_DP  _UA/2 //((filt == 0) ? _UA : (filt == 3) ?  _UA/4 : _UA/2)     //(_UA/2) // the occupied SSB bandwidth can be further reduced by restricting the maximum phase change (set MAX_DP to _UA/2).
+
+#define F_ADC_CONV (192307/2)  //was 192307/1, but as noted this produces clicks in audio stream. Slower ADC clock cures this (but is a problem for VOX when sampling mic-input simulatanously).
+#define F_SAMP_PWM (78125/1)
+
+#define CARRIER_COMPLETELY_OFF_ON_LOW  1    // disable oscillator on low amplitudes, to prevent potential unwanted biasing/leakage through PA circuit
+#define MULTI_ADC  1  // multiple ADC conversions for more sensitive (+12dB) microphone input
+#define MIC_ATTEN 10 // 0*6dB attenuation (note that the LSB bits are quite noisy)
+
+
+
+inline int16_t arctan3(int16_t q, int16_t i)  // error ~ 0.8 degree
+{ // source: [1] http://www-labs.iro.umontreal.ca/~mignotte/IFT2425/Documents/EfficientApproximationArctgFunction.pdf
+//#define _atan2(z)  (_UA/8 + _UA/44) * z  // very much of a simplification...not accurate at all, but fast
+#define _atan2(z)  (_UA/8 + _UA/22 - _UA/22 * z) * z  //derived from (5) [1]   note that atan2 can overflow easily so keep _UA low
+//#define _atan2(z)  (_UA/8 + _UA/24 - _UA/24 * z) * z  //derived from (7) [1]
+  int16_t r;
+  if(abs(q) > abs(i))
+    r = _UA / 4 - _atan2(abs(i) / abs(q));        // arctan(z) = 90-arctan(1/z)
+  else
+    r = (i == 0) ? 0 : _atan2(abs(q) / abs(i));   // arctan(z)
+  r = (i < 0) ? _UA / 2 - r : r;                  // arctan(-z) = -arctan(z)
+  return (q < 0) ? -r : r;                        // arctan(-z) = -arctan(z)
+}
+
+#define magn(i, q) (abs(i) > abs(q) ? abs(i) + abs(q) / 4 : abs(q) + abs(i) / 4) // approximation of: magnitude = sqrt(i*i + q*q); error 0.95dB
+
+
+uint8_t lut[256];
+volatile uint8_t amp;
+//#define MORE_MIC_GAIN   1       // adds more microphone gain, improving overall SSB quality (when speaking further away from microphone)
+#ifdef MORE_MIC_GAIN
+volatile uint8_t vox_thresh = (1 << 2);
+#else
+volatile uint8_t vox_thresh = (1 << 1); //(1 << 2);
+#endif
+volatile uint8_t drive = 2;   // hmm.. drive>2 impacts cpu load..why?
+
+volatile uint8_t quad = 0;
+
+
+inline int16_t ssb(int16_t in)
+{
+  static int16_t dc, z1;
+
+  int16_t i, q;
+  uint8_t j;
+  static int16_t v[16];
+  for(j = 0; j != 15; j++) v[j] = v[j + 1];
+#ifdef MORE_MIC_GAIN
+//#define DIG_MODE  // optimization for digital modes: for super flat TX spectrum, (only down < 100Hz to cut-off DC components)
+  int16_t ac = in * 2;             //   6dB gain (justified since lpf/hpf is losing -3dB)
+  ac = ac + z1;                    // lpf
+  z1 = (in - (2) * z1) / (2 + 1);  // lpf: notch at Fs/2 (alias rejecting)
+  dc = (ac + (2) * dc) / (2 + 1);  // hpf: slow average
+  v[15] = (ac - dc);               // hpf (dc decoupling)
+  i = v[7] * 2;  // 6dB gain for i, q  (to prevent quanitization issues in hilbert transformer and phase calculation, corrected for magnitude calc)
+  q = ((v[0] - v[14]) * 2 + (v[2] - v[12]) * 8 + (v[4] - v[10]) * 21 + (v[6] - v[8]) * 16) / 64 + (v[6] - v[8]); // Hilbert transform, 40dB side-band rejection in 400..1900Hz (@4kSPS) when used in image-rejection scenario; (Hilbert transform require 5 additional bits)
+
+  uint16_t _amp = magn(i / 2, q / 2);  // -6dB gain (correction)
+#else  // !MORE_MIC_GAIN
+  //dc += (in - dc) / 2;       // fast moving average
+  dc = (in + dc) / 2;        // average
+  int16_t ac = (in - dc);   // DC decoupling
+  //v[15] = ac;// - z1;        // high-pass (emphasis) filter
+  v[15] = (ac + z1);// / 2;           // low-pass filter with notch at Fs/2
+  z1 = ac;
+
+  i = v[7];
+  q = ((v[0] - v[14]) * 2 + (v[2] - v[12]) * 8 + (v[4] - v[10]) * 21 + (v[6] - v[8]) * 15) / 128 + (v[6] - v[8]) / 2; // Hilbert transform, 40dB side-band rejection in 400..1900Hz (@4kSPS) when used in image-rejection scenario; (Hilbert transform require 5 additional bits)
+
+  uint16_t _amp = magn(i, q);
+#endif  // MORE_MIC_GAIN
+
+  _amp = _amp << (drive);
+  _amp = ((_amp > 255) || (drive == 8)) ? 255 : _amp; // clip or when drive=8 use max output
+  amp = lut[_amp];
+
+  static int16_t prev_phase;
+  int16_t phase = arctan3(q, i);
+
+  int16_t dp = phase - prev_phase;  // phase difference and restriction
+  //dp = (amp) ? dp : 0;  // dp = 0 when amp = 0
+  prev_phase = phase;
+
+  if(dp < 0) dp = dp + _UA; // make negative phase shifts positive: prevents negative frequencies and will reduce spurs on other sideband
+#ifdef QUAD
+  if(dp >= (_UA/2)){ dp = dp - _UA/2; quad = !quad; }
+#endif
+
+#ifdef MAX_DP
+  if(dp > MAX_DP){ // dp should be less than half unit-angle in order to keep frequencies below F_SAMP_TX/2
+    prev_phase = phase - (dp - MAX_DP);  // substract restdp
+    dp = MAX_DP;
+  }
+#endif
+  if(mode == USB)
+    return dp * ( F_SAMP_TX / _UA); // calculate frequency-difference based on phase-difference
+  else
+    return dp * (-F_SAMP_TX / _UA);
+}
+
+// This is the ADC ISR, issued with sample-rate via timer1 compb interrupt.
+// It performs in real-time the ADC sampling, calculation of SSB phase-differences, calculation of SI5351 frequency registers and send the registers to SI5351 over I2C.
+static int16_t _adc;
+void dsp_tx()
+{ // jitter dependent things first
+  
+  test_dsp_count++;
+  
+#ifdef MULTI_ADC  // SSB with multiple ADC conversions:
+  int16_t adc;                         // current ADC sample 10-bits analog input, NOTE: first ADCL, then ADCH
+  adc = ADC;
+ 
+  ADCSRA |= (1 << ADSC);
+  //OCR1BL = amp;                        // submit amplitude to PWM register (actually this is done in advance (about 140us) of phase-change, so that phase-delays in key-shaping circuit filter can settle)
+//  si5351.SendPLLRegisterBulk();       // submit frequency registers to SI5351 over 731kbit/s I2C (transfer takes 64/731 = 88us, then PLL-loopfilter probably needs 50us to stabalize)
+#ifdef QUAD
+#ifdef TX_CLK0_CLK1
+//  si5351.SendRegister(16, (quad) ? 0x1f : 0x0f);  // Invert/non-invert CLK0 in case of a huge phase-change
+//  si5351.SendRegister(17, (quad) ? 0x1f : 0x0f);  // Invert/non-invert CLK1 in case of a huge phase-change
+#else
+//  si5351.SendRegister(18, (quad) ? 0x1f : 0x0f);  // Invert/non-invert CLK2 in case of a huge phase-change
+#endif
+#endif //QUAD
+  OCR1BL = amp;                      // submit amplitude to PWM register (takes about 1/32125 = 31us+/-31us to propagate) -> amplitude-phase-alignment error is about 30-50us
+  adc += ADC;
+ADCSRA |= (1 << ADSC);  // causes RFI on QCX-SSB units (not on units with direct biasing); ENABLE this line when using direct biasing!!
+  int16_t df = ssb(_adc >> MIC_ATTEN); // convert analog input into phase-shifts (carrier out by periodic frequency shifts)
+  adc += ADC;
+  ADCSRA |= (1 << ADSC);
+//  si5351.freq_calc_fast(df);           // calculate SI5351 registers based on frequency shift and carrier frequency
+  adc += ADC;
+  ADCSRA |= (1 << ADSC);
+  //_adc = (adc/4 - 512);
+#define AF_BIAS   32
+  _adc = (adc/4 - (512 - AF_BIAS));        // now make sure that we keep a postive bias offset (to prevent the phase swapping 180 degrees and potentially causing negative feedback (RFI)
+#else  // SSB with single ADC conversion:
+  ADCSRA |= (1 << ADSC);    // start next ADC conversion (trigger ADC interrupt if ADIE flag is set)
+  OCR1BL = ad;//amp;                        // submit amplitude to PWM register (actually this is done in advance (about 140us) of phase-change, so that phase-delays in key-shaping circuit filter can settle)
+//  si5351.SendPLLRegisterBulk();       // submit frequency registers to SI5351 over 731kbit/s I2C (transfer takes 64/731 = 88us, then PLL-loopfilter probably needs 50us to stabalize)
+  OCR1BL = amp;                        // submit amplitude to PWM register (takes about 1/32125 = 31us+/-31us to propagate) -> amplitude-phase-alignment error is about 30-50us
+  int16_t adc = ADC - 512; // current ADC sample 10-bits analog input, NOTE: first ADCL, then ADCH
+  int16_t df = ssb(adc >> MIC_ATTEN);  // convert analog input into phase-shifts (carrier out by periodic frequency shifts)
+//  si5351.freq_calc_fast(df);           // calculate SI5351 registers based on frequency shift and carrier frequency
+#endif
+
+#ifdef CARRIER_COMPLETELY_OFF_ON_LOW
+//  if(tx == 1){ OCR1BL = 0; si5351.SendRegister(SI_CLK_OE, TX0RX0); }   // disable carrier
+//  if(tx == 255){ si5351.SendRegister(SI_CLK_OE, TX1RX0); } // enable carrier
+#endif
+
+  OCR1AL = df;//(adc << (-1)) + 128;  // TX audio monitoring
+}
+
+
+#define EA(y, x, one_over_alpha)  (y) = (y) + ((x) - (y)) / (one_over_alpha); // exponental averaging [Lyons 13.33.1]
+#define MLEA(y, x, L, M)  (y)  = (y) + ((((x) - (y)) >> (L)) - (((x) - (y)) >> (M))); // multiplierless exponental averaging [Lyons 13.33.1], with alpha=1/2^L - 1/2^M
+
+
+ISR(TIMER2_COMPA_vect)  // Timer2 COMPA interrupt
+{
+  //Serial.print("X");
+  test_count++;
+  dsp_tx(); 
+}
+
+volatile uint8_t admux[3];
+
+#pragma GCC pop_options  // end of DSP section
+
+/*ISR (TIMER2_COMPA_vect  ,ISR_NAKED) {
+asm("push r24         \n\t"
+    "lds r24,  0\n\t"
+    "sts 0xB4, r24    \n\t"
+    "pop r24          \n\t"
+    "reti             \n\t");
+}*/
+
+void adc_start(uint8_t adcpin, bool ref1v1, uint32_t fs)
+{
+  DIDR0 |= (1 << adcpin); // disable digital input 
+  ADCSRA = 0;             // clear ADCSRA register
+  ADCSRB = 0;             // clear ADCSRB register
+  ADMUX = 0;              // clear ADMUX register
+  ADMUX |= (adcpin & 0x0f);    // set analog input pin
+  ADMUX |= ((ref1v1) ? (1 << REFS1) : 0) | (1 << REFS0);  // If reflvl == true, set AREF=1.1V (Internal ref); otherwise AREF=AVCC=(5V)
+  ADCSRA |= ((uint8_t)log2((uint8_t)(F_CPU / 13 / fs))) & 0x07;  // ADC Prescaler (for normal conversions non-auto-triggered): ADPS = log2(F_CPU / 13 / Fs) - 1; ADSP=0..7 resulting in resp. conversion rate of 1536, 768, 384, 192, 96, 48, 24, 12 kHz
+  //ADCSRA |= (1 << ADIE);  // enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN);  // enable ADC
+  //ADCSRA |= (1 << ADSC);  // start ADC measurements
+#ifdef ADC_NR
+//  set_sleep_mode(SLEEP_MODE_ADC);  // ADC NR sleep destroys the timer2 integrity, therefore Idle sleep is better alternative (keeping clkIO as an active clock domain)
+  set_sleep_mode(SLEEP_MODE_IDLE);
+  sleep_enable();
+#endif
+}
+
+void adc_stop()
+{
+  //ADCSRA &= ~(1 << ADATE); // disable auto trigger
+  ADCSRA &= ~(1 << ADIE);  // disable interrupts when measurement complete
+  ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);    // 128 prescaler for 9.6kHz
+#ifdef ADC_NR
+  sleep_disable();
+#endif
+  ADMUX = (1 << REFS0);  // restore reference voltage AREF (5V)
+}
+
+void timer1_start(uint32_t fs)
+{  // Timer 1: OC1A and OC1B in PWM mode
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM11); // Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM (non-inverting mode)
+  TCCR1B |= (1 << CS10) | (1 << WGM13) | (1 << WGM12); // Mode 14 - Fast PWM;  CS10: clkI/O/1 (No prescaling)
+  ICR1H = 0x00;
+  ICR1L = min(255, F_CPU / fs);  // PWM value range (fs>78431):  Fpwm = F_CPU / [Prescaler * (1 + TOP)]
+  //TCCR1A |= (1 << COM1A1) | (1 << COM1B1) | (1 << WGM10); // Clear OC1A/OC1B on compare match, set OC1A/OC1B at BOTTOM (non-inverting mode)
+  //TCCR1B |= (1 << CS10) | (1 << WGM12); // Mode 5 - Fast PWM, 8-bit;  CS10: clkI/O/1 (No prescaling)
+  OCR1AH = 0x00;
+  OCR1AL = 0x00;  // OC1A (SIDETONE) PWM duty-cycle (span defined by ICR).
+  OCR1BH = 0x00;
+  OCR1BL = 0x00;  // OC1B (KEY_OUT) PWM duty-cycle (span defined by ICR).
+}
+
+void timer1_stop()
+{
+  OCR1AL = 0x00;
+  OCR1BL = 0x00;
+}
+
+void timer2_start(uint32_t fs)
+{  // Timer 2: interrupt mode
+  ASSR &= ~(1 << AS2);  // Timer 2 clocked from CLK I/O (like Timer 0 and 1)
+  TCCR2A = 0;
+  TCCR2B = 0;
+  TCNT2 = 0;
+  TCCR2A |= (1 << WGM21); // WGM21: Mode 2 - CTC (Clear Timer on Compare Match)
+  TCCR2B |= (1 << CS22);  // Set C22 bits for 64 prescaler
+  TIMSK2 |= (1 << OCIE2A);  // enable timer compare interrupt TIMER2_COMPA_vect
+  uint8_t ocr = ((F_CPU / 64) / fs) - 1;   // OCRn = (F_CPU / pre-scaler / fs) - 1;
+  OCR2A = ocr;
+}
+
+void timer2_stop()
+{ // Stop Timer 2 interrupt
+  TIMSK2 &= ~(1 << OCIE2A);  // disable timer compare interrupt
+  delay(1);  // wait until potential in-flight interrupts are finished
+}
+
+
+
+
+uint8_t txdelay = 0;
+uint8_t semi_qsk = false;
+uint32_t semi_qsk_timeout = 0;
+
+void switch_rxtx(uint8_t tx_enable){
+  TIMSK2 &= ~(1 << OCIE2A);  // disable timer compare interrupt
+//  dsp_tx();
+//      si5351.SendRegister(SI_CLK_OE, TX1RX0);
+      OCR1AL = 0x80; // make sure SIDETONE is set at 2.5V
+      if(mode != CW) TCCR1A &= ~(1 << COM1A1); // disable SIDETONE, prevent interference during SSB TX
+      TCCR1A |= (1 << COM1B1);  // enable KEY_OUT PWM
+  OCR2A = ((F_CPU / 64) /  F_SAMP_TX ) - 1;
+  TIMSK2 |= (1 << OCIE2A);  // enable timer compare interrupt TIMER2_COMPA_vect
+}
+
+
+static uint8_t pwm_min = 0;    // PWM value for which PA reaches its minimum: 29 when C31 installed;   0 when C31 removed;   0 for biasing BS170 directly
+static uint8_t pwm_max = 255;  // PWM value for which PA reaches its maximum: 96 when C31 installed; 255 when C31 removed;
+
+//refresh LUT based on pwm_min, pwm_max
+void build_lut()
+{
+  for(uint16_t i = 0; i != 256; i++)    // refresh LUT based on pwm_min, pwm_max
+    lut[i] = (i * (pwm_max - pwm_min)) / 255 + pwm_min;
+    //lut[i] = min(pwm_max, (float)106*log(i) + pwm_min);  // compressed microphone output: drive=0, pwm_min=115, pwm_max=220
+}
+
+
+void setup()
+{
+//     s4i5351.powerDown();  // disable all CLK outputs (especially needed for si5351 variants that has CLK2 enabled by default, such as Si5351A-B04486-GT)
+
+  Serial.begin(115200);
+  DDRB |= (1 << PINB1);
+  DDRB |= (1 << PINB2);
+  
+  MCUSR = 0;
+//  wdt_enable(WDTO_4S);  // Enable watchdog
+  uint32_t t0, t1;
+  ADMUX = (1 << REFS0);  // restore reference voltage AREF (5V)
+  // disable external interrupts
+  PCICR = 0;
+  PCMSK0 = 0;
+  PCMSK1 = 0;
+  PCMSK2 = 0;
+
+  //pinMode(A2, INPUT);
+  delay(3000);           // at least 40ms after power rises above 2.7V before sending commands
+  
+  //if(abs((int32_t)F_XTAL - (int32_t)si5351.fxtal) > 50000){ si5351.fxtal = F_XTAL; }  // if F_XTAL frequency deviates too much with actual setting -> use default
+//  si5351.iqmsa = 0;  // enforce PLL reset
+  freq = vfo[0];
+  mode = vfomode[0];
+
+//  build_lut();
+
+  Serial.print("BASLADIK");
+  adc_start(2, true, F_ADC_CONV*4); admux[2] = ADMUX;  // Note that conversion-rate for TX is factors more
+  timer1_start(F_SAMP_PWM);
+  timer2_start(F_SAMP_TX);  
+  
+  //switch_rxtx(1);
+}
+
+
+void loop()
+{
+  Serial.print(test_count);
+  Serial.print(" ");
+  Serial.println(test_dsp_count);
+  test_count=0;
+  test_dsp_count=0;
+  delay(1000);
+    //switch_rxtx(1);
+}
